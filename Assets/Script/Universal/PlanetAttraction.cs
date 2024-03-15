9cb8646291c52ff5f8af7f877fb12ef4132e9040
Magizian1 <rapeepong.wang@bumail.net>
2024-03-15 13:17:41 +0700

    OnEnable

--- Assets/Script/Universal/PlanetAttraction.cs
+++ Assets/Script/Universal/PlanetAttraction.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlanetAttraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Rigidbody rb;
+
+    private const float G = 6.67f;
+
+    public static List<PlanetAttraction> pAttractions;
+    
+
+    void AttractorFoemular(PlanetAttraction other)
     {
-        
+        Rigidbody rbOther = other.rb;
+
+        Vector3 direction = rb.position - rbOther.position;
+
+        float distance = direction.magnitude;
+
+        // F = G * (M1 * M2) / D^2
+        float forceMegnitude = G * (rb.mass * rbOther.mass) / Mathf.Pow(distance , 2);
+
+        Vector3 forceDir = direction.normalized * forceMegnitude;
+        rbOther.AddForce(forceDir);
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        foreach (var attraction in pAttractions)
+        {
+            if (attraction != this)
+            {
+                AttractorFoemular(attraction);
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (pAttractions == null)
+        {
+            pAttractions = new List<PlanetAttraction>();
+        }
+        pAttractions.Add(this);
     }
 }
